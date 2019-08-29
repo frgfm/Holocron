fix: Fixed Lookahead
Fixed params duplication and reimplemented Optimizer methods to better leverage the inherited methods

holocron/optim/lookahead.py
@@ -3,10 +3,9 @@
 
 '''
 Lookahead optimizer wrapper
-Inspired by https://github.com/lonePatient/lookahead_pytorch/blob/master/optimizer.py
 '''
 
-import itertools as it
+from collections import defaultdict
 from torch.optim.optimizer import Optimizer
 
 
@@ -15,25 +14,47 @@ class Lookahead(Optimizer):
 
     Args:
         base_optimizer (torch.optim.optimizer.Optimizer): base parameter optimizer
-        alpha (int, optional): rate of weight synchronization
-        k (int, optional): number of step performed on fast weights before weight synchronization
+        sync_rate (int, optional): rate of weight synchronization
+        sync_period (int, optional): number of step performed on fast weights before weight synchronization
     """
 
-    def __init__(self, base_optimizer, alpha=0.5, k=6):
-        if alpha < 0 or alpha > 1:
-            raise ValueError(f'expected positive float lower than 1 as synchronization rate alpha, received: {alpha}')
-        if not isinstance(k, int) or k < 1:
-            raise ValueError(f'expected positive integer as synchronization period k, received: {k}')
+    def __init__(self, base_optimizer, sync_rate=0.5, sync_period=6):
+        if sync_rate < 0 or sync_rate > 1:
+            raise ValueError(f'expected positive float lower than 1 as sync_rate, received: {sync_rate}')
+        if not isinstance(sync_period, int) or sync_period < 1:
+            raise ValueError(f'expected positive integer as sync_period, received: {sync_period}')
+        # Optimizer attributes
+        self.defaults = dict(sync_rate=sync_rate, sync_period=sync_period)
+        self.state = defaultdict(dict)
+        # Base optimizer attributes
         self.base_optimizer = base_optimizer
-        self.param_groups = self.base_optimizer.param_groups
-        self.alpha = alpha
-        self.k = k
-        self.step_counter = 0
-        # Slow parameters
-        self.slow_groups = [[p.clone().detach() for p in group['params']]
-                             for group in self.param_groups]
-        for w in it.chain(*self.slow_groups):
-            w.requires_grad = False
+        # Wrapper attributes
+        self.fast_steps = 0
+        self.param_groups = []
+        for group in self.base_optimizer.param_groups:
+            self._add_param_group(group)
+
+    def __getstate__(self):
+        return {
+            'defaults': self.defaults,
+            'state': self.state,
+            'base_state': self.base_optimizer.__getstate__(),
+            'fast_steps': self.fast_steps,
+            'param_groups': self.param_groups
+        }
+
+    def state_dict(self):
+        return dict(**super(Lookahead, self).state_dict(),
+                    base_state_dict=self.base_optimizer.state_dict())
+
+    def load_state_dict(self, state_dict):
+        self.base_optimizer.load_state_dict(state_dict['base_state_dict'])
+        super(Lookahead, self).load_state_dict(state_dict)
+        # Update last key of class dict
+        self.__setstate__({'base_state_dict': self.base_optimizer.state_dict()})
+
+    def zero_grad(self):
+        self.base_optimizer.zero_grad()
 
     def step(self, closure=None):
         """Performs a single optimization step.
@@ -41,38 +62,43 @@ class Lookahead(Optimizer):
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
         """
-        loss = None
-        if closure is not None:
-            loss = closure()
 
         # Update fast params
-        loss = self.base_optimizer.step()
-        self.step_counter += 1
-        # Synchronization every k steps on fast params
-        if self.step_counter % self.k == 0:
-            for fast_group, slow_group in zip(self.param_groups, self.slow_groups):
-                for fast_p, slow_p in zip(fast_group['params'], slow_group):
+        loss = self.base_optimizer.step(closure)
+        self.fast_steps += 1
+        # Synchronization every sync_period steps on fast params
+        if self.fast_steps % self.defaults['sync_period'] == 0:
+            for fast_group, slow_group in zip(self.base_optimizer.param_groups, self.param_groups):
+                for fast_p, slow_p in zip(fast_group['params'], slow_group['params']):
                     # Outer update
-                    slow_p.data.add_(self.alpha, fast_p.data - slow_p.data)
+                    slow_p.data.add_(self.defaults['sync_rate'], fast_p.data - slow_p.data)
                     # Synchronize fast and slow params
                     fast_p.data.copy_(slow_p.data)
 
         return loss
 
-    def __getstate__(self):
-        return self.base_optimizer.__getstate__()
-
-    def __setstate__(self, state):
-        self.base_optimizer.__setstate__(state)
-
-    def load_state_dict(self, state_dict):
-        self.base_optimizer.load_state_dict(state_dict)
-
     def __repr__(self):
         format_string = self.__class__.__name__ + ' ('
         optimizer_repr = self.base_optimizer.__repr__().replace('\n', '\n\t')
         format_string += f"\nbase_optimizer={optimizer_repr},"
-        format_string += f"\nalpha={self.alpha},"
-        format_string += f"\nk={self.k}"
+        for arg, val in self.defaults.items():
+            format_string += f"\n{arg}={val},"
         format_string += '\n)'
         return format_string
+
+    def _add_param_group(self, param_group):
+        # Clone & detach params from base optimizer
+        group = dict(params=[p.clone().detach()
+                             for p in param_group['params']],
+                     lr=param_group['lr'])
+        # Uneeded grads
+        for p in group['params']:
+            p.reguires_grad = False
+        self.param_groups.append(group)
+
+    def add_param_group(self, param_group):
+        # Add param group to base optimizer
+        self.base_optimizer.add_param_group(param_group)
+
+        # Add the corresponding slow param group
+        self._add_param_group(self.base_optimizer.param_groups[-1])