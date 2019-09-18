0e4841bd2434958412430f3f496806b3c9ce3464
F-G Fernandez <fgfm03@hotmail.fr>
2019-09-19 01:01:51 +0200

    chore: Added models module tests

---
+++ test/test_models.py
@@ -0,0 +1,27 @@
+import unittest
+import torch
+from holocron.models import res2net
+
+
+class Tester(unittest.TestCase):
+
+    def _test_res2nets(self, name, input_shape):
+        # passing num_class equal to a number other than 1000 helps in making the test
+        # more enforcing in nature
+        model = res2net.__dict__[name](depth=50, num_classes=50, pretrained=True)
+        model.eval()
+        x = torch.rand(input_shape)
+        out = model(x)
+        self.assertEqual(out.shape[-1], 50)
+
+
+for model_name in ['res2net', 'res2next']:
+    def do_test(self, model_name=model_name):
+        input_shape = (4, 3, 224, 224)
+        self._test_res2nets(model_name, input_shape)
+
+    setattr(Tester, "test_" + model_name, do_test)
+
+
+if __name__ == '__main__':
+    unittest.main()
