4ec0a91d005f740066e4cdebd6b2490469f8a9a4
F-G Fernandez <fgfm03@hotmail.fr>
2020-04-29 23:49:44 +0200

    test: Added unittest for darknet19

--- test/test_models.py
+++ test/test_models.py
@@ -14,11 +14,28 @@ class Tester(unittest.TestCase):
         out = model(x)
         self.assertEqual(out.shape[-1], 50)
 
+    def _test_classification_model(self, name):
+
+        num_classes = 10
+        x = torch.rand((2, 3, 224, 224))
+        model = models.__dict__[name](num_classes=num_classes).eval()
+        with torch.no_grad():
+            out = model(x)
+
+        self.assertEqual(out.shape[0], x.shape[0])
+        self.assertEqual(out.shape[-1], num_classes)
+
 
 for model_name in ['res2net', 'res2next']:
     def do_test(self, model_name=model_name):
         input_shape = (4, 3, 224, 224)
         self._test_res2nets(model_name, input_shape)
+
+    setattr(Tester, "test_" + model_name, do_test)
+
+for model_name in ['darknet19']:
+    def do_test(self, model_name=model_name):
+        self._test_classification_model(model_name)
 
     setattr(Tester, "test_" + model_name, do_test)
 
