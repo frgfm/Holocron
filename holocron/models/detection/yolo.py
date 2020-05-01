fix: Fixed loss computation

holocron/models/detection/yolo.py
@@ -7,7 +7,7 @@ Personal implementation of YOLOv2
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.ops.boxes import nms
+from torchvision.ops.boxes import box_iou, nms
 
 from ...nn import ConcatDownsample2d
 from ..darknet import conv1x1, conv3x3, DarknetBody
@@ -53,10 +53,6 @@ class YOLOv2(nn.Module):
     @property
     def num_anchors(self):
         return self.anchors.shape[0]
-
-    @property
-    def loss(self):
-        return self.objectness_loss + 5 * self.bbox_loss + self.clf_loss
 
     def _format_outputs(self, x, img_h, img_w):
         """Formats convolutional layer output
@@ -110,50 +106,50 @@ class YOLOv2(nn.Module):
         """
 
         # Reset losses
-        self.objectness_loss = torch.zeros(1).to(device=self.pred_boxes.device)
-        self.bbox_loss = torch.zeros(1).to(device=self.pred_boxes.device)
-        self.clf_loss = torch.zeros(1).to(device=self.pred_boxes.device)
-        # Assign in each cell, get the box predictor with highest IoU over GT
-        num_boxes = 0
+        objectness_loss = torch.zeros(1).to(pred_boxes.device)
+        bbox_loss = torch.zeros(1).to(pred_boxes.device)
+        clf_loss = torch.zeros(1).to(pred_boxes.device)
         # Convert from x, y, w, h to xmin, ymin, xmax, ymax
         pred_boxes[..., 2:] += pred_boxes[..., :2]
         # B * cells * predictors * info
         for idx in range(pred_boxes.shape[0]):
+
+            # cells * predictors * num_gt
             iou_mat = box_iou(pred_boxes[idx].view(-1, 4), gt_boxes[idx])
             # Assign in each cell the best box predictors
             # Compute max IoU for each predictor, take the highest predictor in each cell
-            tmp_max = iou_mat.max(dim=1).values.view(-1, self.num_anchors).max(dim=1)
-            # Ensure the predictor has a positive IoU
-            has_obj = tmp_max.values > 0
-            selection = list(zip(torch.where(has_obj), tmp_max.indices[has_obj]))
-            selected_pred_boxes = pred_boxes[idx][selection]
-            selected_o = pred_o[idx][selection]
-            selected_scores = pred_scores[idx][selection]
-            # Select the GT with highest IoU
-            tmp_iou = box_iou(selected_pred_boxes, gt_boxes[idx])
-            selection = selection_iou.argmax(dim=1)
-            selected_gt_boxes = gt_boxes[idx][selection]
-            select_gt_labels = gt_labels[idx][selection]
+            cell_selection = iou_mat.view(pred_boxes.shape[1], -1).max(dim=1).values > 0
+            if torch.any(cell_selection):
+                # S * predictors * num_gt
+                iou_mat = iou_mat.view(pred_boxes.shape[1], self.num_anchors, -1)[cell_selection]
+                anchor_selection = iou_mat.max(dim=2).values.argmax(dim=1)
+                selection = [idx * self.num_anchors + anchor.item() for idx, anchor in enumerate(anchor_selection)]
+                # Predictor selection
+                selected_pred_boxes = pred_boxes[idx, cell_selection].view(-1, 4)[selection]
+                selected_o = pred_o[idx, cell_selection].view(-1)[selection]
+                selected_scores = pred_scores[idx, cell_selection].view(-1, self.num_classes)[selection]
+                # GT selection
+                max_iou = iou_mat.view(-1, gt_boxes[idx].shape[0])[selection].max(dim=1)
+                selected_gt_boxes = gt_boxes[idx][max_iou.indices]
+                select_gt_labels = gt_labels[idx][max_iou.indices]
 
-            # Objectness loss
-            self.objectness_loss += F.mse_loss(selected_o, tmp_iou[:, selection], reduction='sum')
-            # Cells where no object was detected
-            empty_cell_o = pred_o[idx][~has_obj].max(dim=1).values
-            self.objectness_loss += 0.5 * F.mse_loss(empty_cell_o, torch.zeros_like(empty_cell_o), reduction='sum')
-            # Regression loss
-            # cf. YOLOv1 loss: SSE of xy preds, SSE of squared root of wh
-            self.bbox_loss += F.mse_loss(selected_pred_boxes[..., :2], selected_gt_boxes[..., :2], reduction='sum')
-            self.bbox_loss += F.mse_loss(selected_pred_boxes[..., 2:].sqrt(), selected_gt_boxes[..., 2:].sqrt(),
-                                         reduction='sum')
-            # Classification loss
-            self.clf_loss += F.cross_entropy(selected_scores, select_gt_labels, reduction='sum')
-            # Box count update
-            num_boxes += len(selection)
+                # Objectness loss for cells where any object was detected
+                objectness_loss += F.mse_loss(selected_o, max_iou.values, reduction='sum')
+                # Regression loss
+                # cf. YOLOv1 loss: SSE of xy preds, SSE of squared root of wh
+                bbox_loss += F.mse_loss(selected_pred_boxes[..., :2], selected_gt_boxes[..., :2], reduction='sum')
+                bbox_loss += F.mse_loss(selected_pred_boxes[..., 2:].sqrt(), selected_gt_boxes[..., 2:].sqrt(),
+                                        reduction='sum')
+                # Classification loss
+                clf_loss += F.cross_entropy(selected_scores, select_gt_labels, reduction='sum')
 
-        # Divide by number of boxes
-        self.objectness_loss /= num_boxes
-        self.bbox_loss /= num_boxes
-        self.clf_loss /= num_boxes
+            # Objectness loss for cells where no object was detected
+            empty_cell_o = pred_o[idx][~cell_selection].max(dim=1).values
+            objectness_loss += 0.5 * F.mse_loss(empty_cell_o, torch.zeros_like(empty_cell_o), reduction='sum')
+
+        return dict(objectness_loss=objectness_loss,
+                    bbox_loss=bbox_loss,
+                    clf_loss=clf_loss)
 
     @staticmethod
     def post_process(b_coords, b_o, b_scores, rpn_nms_thresh=0.7, box_score_thresh=0.05):
@@ -232,7 +228,7 @@ class YOLOv2(nn.Module):
 
         if self.training:
             # Update losses
-            self._compute_losses(b_coords, b_o, b_scores, gt_boxes, gt_labels)
+            return self._compute_losses(b_coords, b_o, b_scores, gt_boxes, gt_labels)
 
         # B * (H * W * num_anchors)
         b_coords = b_coords.view(b_coords.shape[0], -1, 4)