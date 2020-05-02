51aabfbefaa9b9f4d46ebc9065a7b84afcc1e502
F-G Fernandez <fgfm03@hotmail.fr>
2020-05-02 23:39:36 +0200

    feat: Added implementation of YOLOv1

--- holocron/models/detection/yolo.py
+++ holocron/models/detection/yolo.py
@@ -10,85 +10,13 @@ import torch.nn.functional as F
 from torchvision.ops.boxes import box_iou, nms
 
 from ...nn import ConcatDownsample2d
-from ..darknet import conv1x1, conv3x3, DarknetBodyV2
+from ..darknet import conv1x1, conv3x3, DarknetBodyV1, DarknetBodyV2
 
 
-__all__ = ['YOLOv2', 'yolov2']
+__all__ = ['YOLOv1', 'YOLOv2', 'yolov1', 'yolov2']
 
 
-class YOLOv2(nn.Module):
-
-    def __init__(self, layout, num_classes=20, anchors=None):
-
-        super().__init__()
-
-        # Priors computed using K-means
-        if anchors is None:
-            anchors = torch.tensor([[1.08, 1.19], [3.42, 4.41], [6.63, 11.38], [9.42, 5.11], [16.62, 10.52]])
-        self.num_classes = num_classes
-
-        self.backbone = DarknetBodyV2(layout, passthrough=True)
-
-        self.reorg_layer = ConcatDownsample2d(scale_factor=2)
-
-        self.block5 = nn.Sequential(
-            conv3x3(layout[-1][0], layout[-1][0]),
-            nn.BatchNorm2d(layout[-1][0]),
-            nn.LeakyReLU(0.1, inplace=True),
-            conv3x3(layout[-1][0], layout[-1][0]),
-            nn.BatchNorm2d(layout[-1][0]),
-            nn.LeakyReLU(0.1, inplace=True))
-
-        self.block6 = nn.Sequential(
-            conv3x3(layout[-1][0] + layout[-2][0] * 2 ** 2, layout[-1][0]),
-            nn.BatchNorm2d(layout[-1][0]),
-            nn.LeakyReLU(0.1, inplace=True))
-
-        # Each box has P_objectness, 4 coords, and score for each class
-        self.head = conv1x1(layout[-1][0], anchors.shape[0] * (5 + num_classes))
-
-        # Register losses
-        self.register_buffer('anchors', anchors)
-
-    @property
-    def num_anchors(self):
-        return self.anchors.shape[0]
-
-    def _format_outputs(self, x, img_h, img_w):
-        """Formats convolutional layer output
-
-        Args:
-            x (torch.Tensor[N, num_anchors * (5 + num_classes), H, W]): output tensor
-            img_h (int): input image height
-            img_w (int): input image width
-
-        Returns:
-            torch.Tensor[N, H * W, num_anchors, 4]: relative coordinates in format (x, y, w, h)
-            torch.Tensor[N, H * W, num_anchors]: objectness scores
-            torch.Tensor[N, H * W, num_anchors, num_classes]: classification scores
-        """
-
-        b, c, h, w = x.shape
-        # B * C * H * W --> B * H * W * num_anchors * (5 + num_classes)
-        x = x.view(b, self.num_anchors, 5 + self.num_classes, h, w).permute(0, 3, 4, 1, 2)
-        # Cell offset
-        c_x = torch.arange(0, w, dtype=torch.float) * img_w / w
-        c_y = torch.arange(0, h, dtype=torch.float) * img_h / h
-        # Box coordinates
-        b_x = (torch.sigmoid(x[..., 0]) + c_x.view(1, 1, -1, 1)).view(b, -1, self.anchors.shape[0])
-        b_y = (torch.sigmoid(x[..., 1]) + c_y.view(1, -1, 1, 1)).view(b, -1, self.anchors.shape[0])
-        # B * H * W * num_anchors * (5 + num_classes) --> B * (H * W) * num_anchors * (5 + num_classes)
-        x = x.view(b, h * w, self.num_anchors, -1)
-        b_w = img_w / w * (self.anchors[:, 0].view(1, 1, -1) * torch.exp(x[..., 2]))
-        b_h = img_h / h * (self.anchors[:, 1].view(1, 1, -1) * torch.exp(x[..., 3]))
-        # B * (H * W) * num_anchors * 4
-        b_coords = torch.stack((b_x, b_y, b_w, b_h), dim=3)
-        # Objectness
-        b_o = torch.sigmoid(x[..., 4])
-        # Classification scores
-        b_scores = x[..., 5:]
-
-        return b_coords, b_o, b_scores
+class _YOLO(nn.Module):
 
     def _compute_losses(self, pred_boxes, pred_o, pred_scores, gt_boxes, gt_labels):
         """Computes the detector losses as described in `"You Only Look Once: Unified, Real-Time Object Detection"
@@ -198,6 +126,181 @@ class YOLOv2(nn.Module):
 
         return detections
 
+
+class YOLOv1(_YOLO):
+
+    def __init__(self, layout, num_classes=20, num_anchors=2):
+
+        super().__init__()
+
+        self.backbone = DarknetBodyV1(layout)
+
+        self.block4 = nn.Sequential(
+            conv3x3(1024, 1024),
+            conv3x3(1024, 1024, stride=2),
+            conv3x3(1024, 1024),
+            conv3x3(1024, 1024))
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024 * 7 ** 2, 4096),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(4096, 7 ** 2 * (num_anchors * 5 + num_classes)))
+        self.num_anchors = num_anchors
+        self.num_classes = num_classes
+
+    def _format_outputs(self, x, img_h, img_w):
+        """Formats convolutional layer output
+
+        Args:
+            x (torch.Tensor[N, num_anchors * (5 + num_classes), H, W]): output tensor
+            img_h (int): input image height
+            img_w (int): input image width
+
+        Returns:
+            torch.Tensor[N, H * W, num_anchors, 4]: relative coordinates in format (x, y, w, h)
+            torch.Tensor[N, H * W, num_anchors]: objectness scores
+            torch.Tensor[N, H * W, num_anchors, num_classes]: classification scores
+        """
+
+        b, c = x.shape
+        h, w = 7, 7
+        # B * (H * W * (num_anchors * 5 + num_classes)) --> B * H * W * (num_anchors * 5 + num_classes)
+        x = x.view(b, h, w, self.num_anchors * 5 + self.num_classes)
+        # Classification scores
+        b_scores = x[..., -self.num_classes:].view(b, h * w, -1)
+        # Repeat for anchors to keep compatibility across YOLO versions
+        b_scores = b_scores.unsqueeze(2).repeat_interleave(self.num_anchors, dim=2)
+        #  B * H * W * (num_anchors * 5 + num_classes) -->  B * H * W * num_anchors * 5
+        x = x[..., :self.num_anchors * 5].view(b, h, w, self.num_anchors, 5)
+        # Cell offset
+        c_x = torch.arange(0, w, dtype=torch.float) * img_w / w
+        c_y = torch.arange(0, h, dtype=torch.float) * img_h / h
+        # Box coordinates
+        b_x = (torch.sigmoid(x[..., 0]) + c_x.view(1, 1, -1, 1)).view(b, -1, self.num_anchors)
+        b_y = (torch.sigmoid(x[..., 1]) + c_y.view(1, -1, 1, 1)).view(b, -1, self.num_anchors)
+        # B * H * W * num_anchors * (5 + num_classes) --> B * (H * W) * num_anchors * (5 + num_classes)
+        # x = x.view(b, h * w, self.num_anchors, -1)
+        b_w = torch.sigmoid(x[..., 2]).view(b, -1, self.num_anchors)
+        b_h = torch.sigmoid(x[..., 3]).view(b, -1, self.num_anchors)
+        # B * (H * W) * num_anchors * 4
+        b_coords = torch.stack((b_x, b_y, b_w, b_h), dim=3)
+        # Objectness
+        b_o = torch.sigmoid(x[..., 4]).view(b, -1, self.num_anchors)
+
+        return b_coords, b_o, b_scores
+
+    def forward(self, x, gt_boxes=None, gt_labels=None):
+        """Perform detection on an image tensor and returns either the loss dictionary in training mode
+        or the list of detections in eval mode.
+
+        Args:
+            x (torch.Tensor[N, 3, H, W]): input image tensor
+            gt_boxes (list<torch.Tensor[-1, 4]>, optional): ground truth boxes relative coordinates
+            in format [xmin, ymin, xmax, ymax]
+            gt_labels (list<torch.Tensor[-1]>, optional): ground truth labels
+        """
+
+        if self.training and (gt_boxes is None or gt_labels is None):
+            raise ValueError("`gt_boxes` and `gt_labels` need to be specified in training mode")
+
+        img_h, img_w = x.shape[-2:]
+        x = self.backbone(x)
+        x = self.block4(x)
+        x = self.classifier(x)
+
+        # B * (H * W) * num_anchors
+        b_coords, b_o, b_scores = self._format_outputs(x, img_h, img_w)
+
+        if self.training:
+            # Update losses
+            return self._compute_losses(b_coords, b_o, b_scores, gt_boxes, gt_labels)
+        else:
+            # B * (H * W * num_anchors)
+            b_coords = b_coords.view(b_coords.shape[0], -1, 4)
+            b_o = b_o.view(b_o.shape[0], -1)
+            b_scores = b_scores.contiguous().view(b_scores.shape[0], -1, self.num_classes)
+
+            # Stack detections into a list
+            return self.post_process(b_coords, b_o, b_scores)
+
+        return x
+
+
+class YOLOv2(_YOLO):
+
+    def __init__(self, layout, num_classes=20, anchors=None):
+
+        super().__init__()
+
+        # Priors computed using K-means
+        if anchors is None:
+            anchors = torch.tensor([[1.08, 1.19], [3.42, 4.41], [6.63, 11.38], [9.42, 5.11], [16.62, 10.52]])
+        self.num_classes = num_classes
+
+        self.backbone = DarknetBodyV2(layout, passthrough=True)
+
+        self.reorg_layer = ConcatDownsample2d(scale_factor=2)
+
+        self.block5 = nn.Sequential(
+            conv3x3(layout[-1][0], layout[-1][0]),
+            nn.BatchNorm2d(layout[-1][0]),
+            nn.LeakyReLU(0.1, inplace=True),
+            conv3x3(layout[-1][0], layout[-1][0]),
+            nn.BatchNorm2d(layout[-1][0]),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        self.block6 = nn.Sequential(
+            conv3x3(layout[-1][0] + layout[-2][0] * 2 ** 2, layout[-1][0]),
+            nn.BatchNorm2d(layout[-1][0]),
+            nn.LeakyReLU(0.1, inplace=True))
+
+        # Each box has P_objectness, 4 coords, and score for each class
+        self.head = conv1x1(layout[-1][0], anchors.shape[0] * (5 + num_classes))
+
+        # Register losses
+        self.register_buffer('anchors', anchors)
+
+    @property
+    def num_anchors(self):
+        return self.anchors.shape[0]
+
+    def _format_outputs(self, x, img_h, img_w):
+        """Formats convolutional layer output
+
+        Args:
+            x (torch.Tensor[N, num_anchors * (5 + num_classes), H, W]): output tensor
+            img_h (int): input image height
+            img_w (int): input image width
+
+        Returns:
+            torch.Tensor[N, H * W, num_anchors, 4]: relative coordinates in format (x, y, w, h)
+            torch.Tensor[N, H * W, num_anchors]: objectness scores
+            torch.Tensor[N, H * W, num_anchors, num_classes]: classification scores
+        """
+
+        b, c, h, w = x.shape
+        # B * C * H * W --> B * H * W * num_anchors * (5 + num_classes)
+        x = x.view(b, self.num_anchors, 5 + self.num_classes, h, w).permute(0, 3, 4, 1, 2)
+        # Cell offset
+        c_x = torch.arange(0, w, dtype=torch.float) * img_w / w
+        c_y = torch.arange(0, h, dtype=torch.float) * img_h / h
+        # Box coordinates
+        b_x = (torch.sigmoid(x[..., 0]) + c_x.view(1, 1, -1, 1)).view(b, -1, self.num_anchors)
+        b_y = (torch.sigmoid(x[..., 1]) + c_y.view(1, -1, 1, 1)).view(b, -1, self.num_anchors)
+        # B * H * W * num_anchors * (5 + num_classes) --> B * (H * W) * num_anchors * (5 + num_classes)
+        x = x.view(b, h * w, self.num_anchors, -1)
+        b_w = img_w / w * (self.anchors[:, 0].view(1, 1, -1) * torch.exp(x[..., 2]))
+        b_h = img_h / h * (self.anchors[:, 1].view(1, 1, -1) * torch.exp(x[..., 3]))
+        # B * (H * W) * num_anchors * 4
+        b_coords = torch.stack((b_x, b_y, b_w, b_h), dim=3)
+        # Objectness
+        b_o = torch.sigmoid(x[..., 4])
+        # Classification scores
+        b_scores = x[..., 5:]
+
+        return b_coords, b_o, b_scores
+
     def forward(self, x, gt_boxes=None, gt_labels=None):
         """Perform detection on an image tensor and returns either the loss dictionary in training mode
         or the list of detections in eval mode.
@@ -238,6 +341,23 @@ class YOLOv2(nn.Module):
 
             # Stack detections into a list
             return self.post_process(b_coords, b_o, b_scores)
+
+
+def yolov1(num_classes=20, num_anchors=2):
+    """YOLO model from
+    `"You Only Look Once: Unified, Real-Time Object Detection" <https://pjreddie.com/media/files/papers/yolo_1.pdf>`_
+
+    Args:
+        num_classes (int, optional): number of output classes
+        anchors (int, optional): number of anchors per grid cell
+
+    Returns:
+        torch.nn.Module: detection module
+    """
+
+    return YOLOv1([[128, 256, 256, 512], [*([256, 512] * 4), 512, 1024], [512, 1024, 512, 1024]],
+                  num_classes, num_anchors)
+
 
 
 def yolov2(num_classes=20, anchors=None):
