80dbc435b65fff5a5dcc15ddbe71674f6c9e4992
F-G Fernandez <fgfm03@hotmail.fr>
2020-05-10 12:42:32 +0200

    refactor: Refactored loss modules

--- holocron/nn/modules/loss.py
+++ holocron/nn/modules/loss.py
@@ -11,7 +11,25 @@ from .. import functional as F
 __all__ = ['FocalLoss', 'LabelSmoothingCrossEntropy']
 
 
-class FocalLoss(nn.Module):
+class _Loss(nn.Module):
+
+    def __init__(self, weight=None, ignore_index=-100, reduction='mean'):
+        super().__init__()
+        self.weight = weight
+        # Cast class weights if possible
+        if isinstance(weight, (float, int)):
+            self.weight = torch.Tensor([weight, 1 - weight])
+        elif isinstance(weight, list):
+            self.weight = torch.Tensor(weight)
+        self.ignore_index = ignore_index
+        # Set the reduction method
+        if reduction not in ['none', 'mean', 'sum']:
+            raise NotImplementedError(f"argument reduction received an incorrect input")
+        else:
+            self.reduction = reduction
+
+
+class FocalLoss(_Loss):
     """Implementation of Focal Loss as described in
     `"Focal Loss for Dense Object Detection" <https://arxiv.org/pdf/1708.02002.pdf>`_
 
@@ -22,21 +40,9 @@ class FocalLoss(nn.Module):
         reduction (str, optional): type of reduction to apply to the final loss
     """
 
-    def __init__(self, gamma=2, weight=None, ignore_index=-100, reduction='mean'):
-        super(FocalLoss, self).__init__()
+    def __init__(self, gamma=2, **kwargs):
+        super().__init__(**kwargs)
         self.gamma = gamma
-        self.weight = weight
-        # Cast class weights if possible
-        if isinstance(weight, (float, int)):
-            self.weight = torch.Tensor([weight, 1 - weight])
-        elif isinstance(weight, list):
-            self.weight = torch.Tensor(weight)
-        self.ignore_index = ignore_index
-        # Set the reduction method
-        if reduction not in ['none', 'mean', 'sum']:
-            raise NotImplementedError(f"argument reduction received an incorrect input")
-        else:
-            self.reduction = reduction
 
     def forward(self, x, target):
         return F.focal_loss(x, target, self.weight, self.ignore_index, self.reduction, self.gamma)
@@ -45,7 +51,7 @@ class FocalLoss(nn.Module):
         return f"{self.__class__.__name__}(gamma={self.gamma}, reduction='{self.reduction}')"
 
 
-class LabelSmoothingCrossEntropy(nn.Module):
+class LabelSmoothingCrossEntropy(_Loss):
     """Implementation of the cross-entropy loss with label smoothing on hard target as described in
     `"Attention Is All You Need" <https://arxiv.org/pdf/1706.03762.pdf>`_
 
@@ -56,21 +62,9 @@ class LabelSmoothingCrossEntropy(nn.Module):
         reduction (str, optional): type of reduction to apply to the final loss
     """
 
-    def __init__(self, eps=0.1, weight=None, ignore_index=-100, reduction='mean'):
-        super(LabelSmoothingCrossEntropy, self).__init__()
+    def __init__(self, eps=0.1, **kwargs):
+        super().__init__(**kwargs)
         self.eps = eps
-        self.weight = weight
-        # Cast class weights if possible
-        if isinstance(weight, (float, int)):
-            self.weight = torch.Tensor([weight, 1 - weight])
-        elif isinstance(weight, list):
-            self.weight = torch.Tensor(weight)
-        self.ignore_index = ignore_index
-        # Set the reduction method
-        if reduction not in ['none', 'mean', 'sum']:
-            raise NotImplementedError(f"argument reduction received an incorrect input")
-        else:
-            self.reduction = reduction
 
     def forward(self, x, target):
         return F.ls_cross_entropy(x, target, self.weight, self.ignore_index, self.reduction, self.eps)
