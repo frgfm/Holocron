bebcb2358c8124ed1ecec10f75ffdc60c2e973ab
F-G Fernandez <fgfm03@hotmail.fr>
2020-05-10 17:41:43 +0200

    test: Added unittest for lr_finder

---
+++ test/test_utils.py
@@ -0,0 +1,58 @@
+import unittest
+import torch
+from holocron import utils
+
+
+class MockDataset(torch.utils.data.Dataset):
+    """Mock dataset generating a random sample and a counting target"""
+    def __init__(self, n):
+        super(MockDataset, self).__init__()
+        self.n = n
+
+    def __getitem__(self, idx):
+        return torch.rand(32), torch.tensor(idx, dtype=torch.long)
+
+    def __len__(self):
+        return self.n
+
+
+def train_one_batch(model, x, target, optimizer, criterion, device):
+    """Mock batch training function"""
+
+    x, target = x.to(device), target.to(device)
+    output = model(x)
+    batch_loss = criterion(output, target.view(-1))
+
+    optimizer.zero_grad()
+    batch_loss.backward()
+    optimizer.step()
+
+    return batch_loss.item()
+
+
+class Tester(unittest.TestCase):
+    def test_lr_finder(self):
+
+        num_it = 10
+        batch_size = 2
+        start_lr, end_lr = 1e-7, 10
+        # Generate all dependencies
+        model = torch.nn.Linear(32, 5)
+        train_loader = torch.utils.data.DataLoader(MockDataset(num_it * batch_size), batch_size=batch_size)
+        optimizer = torch.optim.Adam(model.parameters())
+        criterion = torch.nn.CrossEntropyLoss()
+        # Perform the iterations
+        lrs, losses = utils.misc.lr_finder(train_one_batch, model, train_loader, optimizer, criterion,
+                                           num_it=num_it, start_lr=start_lr, end_lr=end_lr)
+
+        # Check integrity of results
+        self.assertIsInstance(lrs, list)
+        self.assertIsInstance(losses, list)
+        self.assertEqual(len(lrs), num_it)
+        self.assertEqual(len(losses), num_it)
+        self.assertEqual(lrs[0], start_lr)
+        self.assertAlmostEqual(lrs[-1], end_lr)
+
+
+if __name__ == '__main__':
+    unittest.main()
