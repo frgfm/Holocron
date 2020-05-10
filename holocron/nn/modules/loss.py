d97982788ed436e5f30eba4bfdffbcb251f7f0c6
F-G Fernandez <fgfm03@hotmail.fr>
2020-05-10 11:37:53 +0200

    feat: Added LabelSmoothingCrossEntropy module

--- holocron/nn/modules/loss.py
+++ holocron/nn/modules/loss.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 from .. import functional as F
 
-__all__ = ['FocalLoss']
+__all__ = ['FocalLoss', 'LabelSmoothingCrossEntropy']
 
 
 class FocalLoss(nn.Module):
@@ -43,3 +43,37 @@ class FocalLoss(nn.Module):
 
     def __repr__(self):
         return f"{self.__class__.__name__}(gamma={self.gamma}, reduction='{self.reduction}')"
+
+
+class LabelSmoothingCrossEntropy(nn.Module):
+    """Implementation of the cross-entropy loss with label smoothing on hard target as described in
+    `"Attention Is All You Need" <https://arxiv.org/pdf/1706.03762.pdf>`_
+
+    Args:
+        eps (float): smoothing factor
+        weight (torch.Tensor[K], optional): class weight for loss computation
+        ignore_index (int, optional): specifies target value that is ignored and do not contribute to gradient
+        reduction (str, optional): type of reduction to apply to the final loss
+    """
+
+    def __init__(self, eps=2, weight=None, ignore_index=-100, reduction='mean'):
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        self.eps = eps
+        self.weight = weight
+        # Cast class weights if possible
+        if isinstance(weight, (float, int)):
+            self.weight = torch.Tensor([weight, 1 - weight])
+        elif isinstance(weight, list):
+            self.weight = torch.Tensor(weight)
+        self.ignore_index = ignore_index
+        # Set the reduction method
+        if reduction not in ['none', 'mean', 'sum']:
+            raise NotImplementedError(f"argument reduction received an incorrect input")
+        else:
+            self.reduction = reduction
+
+    def focal_loss(self, x, target):
+        return F.ls_cross_entropy(x, target, self.weight, self.ignore_index, self.reduction, self.eps)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(eps={self.eps}, reduction='{self.reduction}')"
