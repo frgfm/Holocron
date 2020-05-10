da385c68cf90752fa21803d40084b9577dc5fc13
F-G Fernandez <fgfm03@hotmail.fr>
2020-05-11 01:51:29 +0200

    feat: Added darknets pretrained weights (#29)
    * chore: Updated package version
    
    * feat: Added darknets pretrained weights
    
    * chore: Added setup.cfg
    
    * chore: Added conda recipe
    
    * chore: Added CI job to publish stable releases

--- holocron/models/darknet.py
+++ holocron/models/darknet.py
@@ -20,13 +20,13 @@ __all__ = ['DarknetV1', 'DarknetV2', 'DarknetV3', 'darknet24', 'darknet19', 'dar
 default_cfgs = {
     'darknet24': {'arch': 'DarknetV1',
                   'layout': [[128, 256, 256, 512], [*([256, 512] * 4), 512, 1024], [512, 1024, 512, 1024]],
-                  'url': None},
+                  'url': 'https://github.com/frgfm/Holocron/releases/download/v0.1.0/darknet24_224-c88d3570.pth'},
     'darknet19': {'arch': 'DarknetV2',
                   'layout': [(128, 1), (256, 1), (512, 2), (1024, 2)],
-                  'url': None},
+                  'url': 'https://github.com/frgfm/Holocron/releases/download/v0.1.0/darknet19_224-44aaede3.pth'},
     'darknet53': {'arch': 'DarknetV3',
                   'layout': [(1, 64, 128), (2, 128, 256), (8, 256, 512), (8, 512, 1024), (4, 1024)],
-                  'url': None},
+                  'url': 'https://github.com/frgfm/Holocron/releases/download/v0.1.0/darknet53_224-f4c640a8.pth'},
 }
 
 
