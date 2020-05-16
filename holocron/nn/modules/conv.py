8a40f41c19c1bfae3674ba34b4823cdd37da9277
F-G Fernandez <fgfm03@hotmail.fr>
2020-05-16 18:34:26 +0200

    fix: Fixed padding method
    To be reverted when https://github.com/pytorch/pytorch/commit/5f2a27401506162a9890a99fec4f7a759b30787e is merged

--- holocron/nn/modules/conv.py
+++ holocron/nn/modules/conv.py
@@ -73,7 +73,7 @@ class NormConv2d(_NormConvNd):
 
     def forward(self, input):
         if self.padding_mode != 'zeros':
-            return F.norm_conv2d(pad(input, self._reversed_padding_repeated_twice, mode=self.padding_mode),
+            return F.norm_conv2d(pad(input, self._padding_repeated_twice, mode=self.padding_mode),
                                  self.weight, self.bias, self.stride, _pair(0),
                                  self.dilation, self.groups, self.eps)
         return F.norm_conv2d(input, self.weight, self.bias, self.stride, self.padding,
