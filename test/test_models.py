feat: Fixed YOLO inference and loss computation (#38)
* fix: Fixed device discrepancy in YOLO

* fix: Fixed box formatting

* fix: Fixed YOLO loss

* fix: Ensure YOLO output coords are relative

To prevent overflow for loss computation, convert coordinates to relative

* refactor: Convert GT coordinates to relative

* refactor: YOLO expects relative GT coordinates in training mode

* feat: Extended argument type support of YOLO

Added list and tuple of Tensors as supported inputs of the forward method

* test: Added unittest for argument type support by YOLO

* test: Fixed YOLO argument type support unittest

* fix: Fixed YOLO loss

* fix: Fixed YOLO loss gradient computation

Inplace operation made gradient computation fail

* fix: Fixed box regression loss of YOLO

* fix: Fixed GT coordinate format

* refactor: Stick to loss definition from paper

* test: Fixed YOLO unittest

* test: Added unittest to handle empty GT cases

* fix: Fixed edge case of empty GT

* style: Fixed lint

* fix: Fixed YOLOv1 loss

* fix: Fixed YOLO detection output in eval mode

* fix: Fixed YOLO pred coordinate conversion

* refactor: Refactored YOLO loss

* fix: Fixed localization loss of YOLO

* fix: Fixed YOLO loss

* fix: Fixes prediction shape issues

* test: Added unittest when a grid cell gets 2 GT assignments

* fix: Fixed GT coordinate conversion

* refactor: Refactored tensor indexing and slicing

* fix: Fixed edge case of double assignment on same anchor box

* style: Fixed lint

* feat: Added pretrained_backbone option on YOLO

* test: Added unittest for pretrained_backbone on YOLO

* refactor: Refactored YOLO loss

* fix: Fixed box coordinate conversion

* test: Added unittest for NaN loss

* docs: Updated comment

* fix: Fixed pred box coordinate usage in YOLO loss

* style: Fixed lint

* refactor: Refactored YOLO operations

* refactor: Refactor viewing and reshaping

* refactor: Stick to 

test/test_models.py
@@ -31,6 +31,8 @@ class Tester(unittest.TestCase):
         num_batches = 2
         x = torch.rand((num_batches, 3, size, size))
         model = models.__dict__[name](pretrained=True, num_classes=num_classes).eval()
+        # Check backbone pretrained
+        model = models.__dict__[name](pretrained_backbone=True, num_classes=num_classes).eval()
         with torch.no_grad():
             out = model(x)
 
@@ -41,11 +43,17 @@ class Tester(unittest.TestCase):
             self.assertIsInstance(out[0].get('scores'), torch.Tensor)
             self.assertIsInstance(out[0].get('labels'), torch.Tensor)
 
+        # Check that list of Tensors does not change output
+        x_list = [torch.rand(3, size, size) for _ in range(num_batches)]
+        with torch.no_grad():
+            out_list = model(x_list)
+            self.assertEqual(len(out_list), len(out))
+
         # Training mode without target
         model = model.train()
         self.assertRaises(ValueError, model, x)
         # Generate targets
-        num_boxes = [2, 3]
+        num_boxes = [3, 4]
         gt_boxes = []
         for num in num_boxes:
             _boxes = torch.rand((num, 4), dtype=torch.float)
@@ -54,7 +62,10 @@ class Tester(unittest.TestCase):
             # Ensure some anchors will be assigned
             _boxes[0, :2] = 0
             _boxes[0, 2:] = 1
-        gt_boxes = [torch.rand((num, 4)) for num in num_boxes]
+            # Check cases where cell can get two assignments
+            _boxes[1, :2] = 0.2
+            _boxes[1, 2:] = 0.8
+            gt_boxes.append(_boxes)
         gt_labels = [(num_classes * torch.rand(num)).to(dtype=torch.long) for num in num_boxes]
 
         # Loss computation
@@ -62,6 +73,12 @@ class Tester(unittest.TestCase):
         self.assertIsInstance(loss, dict)
         for subloss in loss.values():
             self.assertIsInstance(subloss, torch.Tensor)
+            self.assertFalse(torch.isnan(subloss))
+
+        # Loss computation with no GT
+        gt_boxes = [torch.zeros((0, 4)) for _ in num_boxes]
+        gt_labels = [torch.zeros(0, dtype=torch.long) for _ in num_boxes]
+        loss = model(x, gt_boxes, gt_labels)
 
 
 for model_name in ['res2net', 'res2next']: