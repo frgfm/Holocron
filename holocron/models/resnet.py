24686a25d90e05d6375fba46a64a5e716e1ea3b2
F-G Fernandez <fgfm03@hotmail.fr>
2020-07-21 23:20:38 +0200

    fix: Fixed weight loading

--- holocron/models/resnet.py
+++ holocron/models/resnet.py
@@ -9,6 +9,7 @@ import logging
 from math import ceil
 from collections import OrderedDict
 import torch.nn as nn
+from torchvision.models.utils import load_state_dict_from_url
 from holocron.nn import SiLU, init
 
 
