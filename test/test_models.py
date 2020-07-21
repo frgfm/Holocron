1cfe06c3ee91c33fbcd866a6b1f7061e03d28387
F-G Fernandez <fgfm03@hotmail.fr>
2020-07-21 13:21:17 +0200

    test: Added unittest

--- test/test_models.py
+++ test/test_models.py
@@ -103,7 +103,8 @@ for model_name in ['res2net', 'res2next']:
 
 for model_name in ['darknet24', 'darknet19', 'darknet53',
                    'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
-                   'resnext50_32x4d', 'resnext101_32x8d']:
+                   'resnext50_32x4d', 'resnext101_32x8d',
+                   'rexnet1_0x']:
     def do_test(self, model_name=model_name):
         self._test_classification_model(model_name)
 
