4d8a3bc56c281061a0ae7d9b9655e86610faba30
F-G Fernandez <fgfm03@hotmail.fr>
2020-07-23 22:55:51 +0200

    feat: Added faster global average pooling implementation

--- holocron/nn/modules/downsample.py
+++ holocron/nn/modules/downsample.py
@@ -7,7 +7,7 @@ Downsampling modules
 import torch.nn as nn
 from .. import functional as F
 
-__all__ = ['ConcatDownsample2d']
+__all__ = ['ConcatDownsample2d', 'GlobalAvgPool2d']
 
 
 class ConcatDownsample2d(nn.Module):
@@ -25,3 +25,22 @@ class ConcatDownsample2d(nn.Module):
     def forward(self, x):
 
         return F.concat_downsample2d(x, self.scale_factor)
+
+
+class GlobalAvgPool2d(nn.Module):
+    """Fast implementation of global average pooling from `"TResNet: High Performance GPU-Dedicated Architecture"
+    <https://arxiv.org/pdf/2003.13630.pdf>`_
+
+    Args:
+        flatten (bool, optional): whether spatial dimensions should be squeezed
+    """
+    def __init__(self, flatten=False):
+        super().__init__()
+        self.flatten = flatten
+
+    def forward(self, x):
+        if self.flatten:
+            in_size = x.size()
+            return x.view((in_size[0], in_size[1], -1)).mean(dim=2)
+        else:
+            return x.view(x.size(0), x.size(1), -1).mean(-1).view(x.size(0), x.size(1), 1, 1)
