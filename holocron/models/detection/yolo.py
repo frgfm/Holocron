45c3cf3d3b6c6bf3d87536c6a6b69b9a6e702174
F-G Fernandez <fgfm03@hotmail.fr>
2020-08-01 16:53:48 +0200

    feat: Updated anchors with paper author's
    cf. https://github.com/pjreddie/darknet/blob/master/cfg/yolov2-voc.cfg#L242

--- holocron/models/detection/yolo.py
+++ holocron/models/detection/yolo.py
@@ -324,7 +324,8 @@ class YOLOv2(_YOLO):
 
         # Priors computed using K-means
         if anchors is None:
-            anchors = torch.tensor([[1.08, 1.19], [3.42, 4.41], [6.63, 11.38], [9.42, 5.11], [16.62, 10.52]])
+            anchors = torch.tensor([[1.3221, 1.73145], [3.19275, 4.00944], [5.05587, 8.09892],
+                                    [9.47112, 4.84053], [11.2364, 10.0071]])
         self.num_classes = num_classes
 
         self.backbone = DarknetBodyV2(layout, in_channels, act_layer, backbone_norm_layer, drop_layer, conv_layer)
