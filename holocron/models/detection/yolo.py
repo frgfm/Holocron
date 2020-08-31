3ab076e64e7dfa0a785faeb26425c717ae9540ea
F-G Fernandez <fgfm03@hotmail.fr>
2020-08-31 05:39:10 +0200

    feat: Added default initialization

--- holocron/models/detection/yolo.py
+++ holocron/models/detection/yolo.py
@@ -545,6 +545,7 @@ class Neck(nn.Module):
 
         self.pan1 = PAN(in_planes[1], act_layer, norm_layer, drop_layer, conv_layer)
         self.pan2 = PAN(in_planes[2], act_layer, norm_layer, drop_layer, conv_layer)
+        init_module(self, 'leaky_relu')
 
     def forward(self, feats):
 
