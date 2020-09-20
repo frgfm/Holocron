f9bf095143cf363dfcce8b953706011d685fe7d4
F-G Fernandez <fgfm03@hotmail.fr>
2020-09-20 14:32:30 +0200

    test: Updated unittests

--- test/test_trainer.py
+++ test/test_trainer.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 
 import unittest
+from tempfile import NamedTemporaryFile
 import torch
 import torch.nn as nn
+from holocron.nn import GlobalAvgPool2d
 from holocron import trainer
+
+
+class MockDataset(torch.utils.data.Dataset):
+    """Mock dataset generating a random sample and a fixed zero target"""
+    def __init__(self, n):
+        super().__init__()
+        self.n = n
+
+    def __getitem__(self, idx):
+        return torch.rand((3, 32, 32)), 0
+
+    def __len__(self):
+        return self.n
 
 
 class UtilsTester(unittest.TestCase):
@@ -20,7 +35,7 @@ class UtilsTester(unittest.TestCase):
             p.requires_grad_(False)
         mod = trainer.freeze_bn(mod)
         for _ in range(10):
-            out = mod(torch.rand((1, 3, 32, 32)))
+            _ = mod(torch.rand((1, 3, 32, 32)))
         # Check that stats were not updated
         self.assertTrue(torch.equal(mod[1].num_batches_tracked, nb))
         self.assertTrue(torch.equal(mod[1].running_mean, rm))
@@ -34,6 +49,40 @@ class UtilsTester(unittest.TestCase):
         # Check that the correct layers were frozen
         self.assertFalse(any(p.requires_grad for p in mod[0].parameters()))
         self.assertTrue(all(p.requires_grad for p in mod[2].parameters()))
+
+
+class CoreTester(unittest.TestCase):
+
+    def test_classification_trainer(self):
+
+        num_it = 100
+        batch_size = 8
+        # Generate all dependencies
+        model = nn.Sequential(nn.Conv2d(3, 32, 3), nn.ReLU(inplace=True),
+                              GlobalAvgPool2d(flatten=True), nn.Linear(32, 5))
+        model_w = model[-1].weight.data.clone()
+        train_loader = torch.utils.data.DataLoader(MockDataset(num_it * batch_size), batch_size=batch_size)
+        optimizer = torch.optim.Adam(model.parameters())
+        criterion = torch.nn.CrossEntropyLoss()
+
+        with NamedTemporaryFile() as tf:
+            learner = trainer.ClassificationTrainer(model, train_loader, train_loader, criterion, optimizer,
+                                                    output_file=tf.name)
+            learner.save(tf.name)
+            # Check setup
+            self.assertTrue(learner.check_setup(num_it=num_it))
+
+            # LR Find
+            learner.load(torch.load(tf.name, map_location='cpu'))
+            learner.lr_find(num_it=num_it)
+            self.assertEqual(len(learner.lr_recorder), len(learner.loss_recorder))
+
+            # Training
+            # Perform the iterations
+            learner.load(torch.load(tf.name, map_location='cpu'))
+            learner.fit_n_epochs(1, 1e-3, None)
+            # Check that params were updated
+            self.assertFalse(torch.equal(model[-1].weight.data, model_w))
 
 
 if __name__ == '__main__':
