b6a463181ce94453c5119ca1d924b163c69ee127
F-G Fernandez <fgfm03@hotmail.fr>
2020-09-20 12:29:41 +0200

    feat: Added trainer module and its utils

--- holocron/__init__.py
+++ holocron/__init__.py
@@ -2,5 +2,6 @@ from holocron import models
 from holocron import nn
 from holocron import ops
 from holocron import optim
+from holocron import trainer
 from holocron import utils
 from .version import __version__
