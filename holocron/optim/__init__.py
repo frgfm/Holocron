704c7e71dcce3ca804505bfc2fa3a5d36217c1fb
F-G Fernandez <fgfm03@hotmail.fr>
2020-10-17 02:03:25 +0200

    feat: Added support of AdaBelief (#93)
    * feat: Added support of AdaBelief
    
    * test: Updated unittests for AdaBelief
    
    * refactor: Refactored RAdam
    
    * refactor: Refactored TAdam
    
    * refactor: Reorder functions
    
    * feat: Added variance rectification option
    
    * refactor: Removed unused var
    
    * docs: Updated documentation
    
    * docs: Updated readme
    
    * refactor: Removed rectification in Adabelief
    
    * docs: Fixed docstring

--- holocron/optim/__init__.py
+++ holocron/optim/__init__.py
@@ -3,6 +3,7 @@ from .lamb import Lamb
 from .radam import RAdam
 from .ralars import RaLars
 from .tadam import TAdam
+from .adabelief import AdaBelief
 from . import wrapper
 from . import lr_scheduler
 
@@ -10,3 +11,5 @@ del lars
 del lamb
 del radam
 del ralars
+del tadam
+del adabelief
