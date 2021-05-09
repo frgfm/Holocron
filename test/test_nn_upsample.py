c858b80cd4a2d0527054cbf2f4f359571a9933ed
frgfm <fgfm03@hotmail.fr>
2021-05-09 13:42:43 +0200

    test: Added unittest for functional form

---
+++ test/test_nn_upsample.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2019-2021, François-Guillaume Fernandez.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
+import pytest
+import torch
+from holocron.nn import functional as F
+
+
+def test_stackupsample2d():
+
+    num_batches = 2
+    num_chan = 4
+    scale_factor = 2
+    x = torch.arange(num_batches * num_chan * 4 ** 2).view(num_batches, num_chan, 4, 4)
+
+    # Test functional API
+    with pytest.raises(AssertionError):
+        F.stack_upsample2d(x, 3)
+
+    # Check that it's the inverse of concat_downsample2d
+    x = torch.rand((num_batches, num_chan, 32, 32))
+    down = F.concat_downsample2d(x, scale_factor=2)
+    assert torch.equal(F.stack_upsample2d(down, scale_factor=2), x)
