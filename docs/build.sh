efde3da07a9d3078c40296c60b79c2de911d3e5d
frgfm <fgfm03@hotmail.fr>
2021-11-01 02:35:56 +0100

    docs: Fix doc building

--- docs/build.sh
+++ docs/build.sh
@@ -6,7 +6,7 @@ function deploy_doc(){
     COMMIT=$(git rev-parse --short HEAD)
     echo "Creating doc at commit" $COMMIT "and pushing to folder $2"
     # Hotfix
-    sed -i "s/torchvision>=0.6.1/torchvision>=0.6.1,<0.11.0/" requirements.txt
+    sed -i "s/^torchvision.*/&,<0.11.0/" requirements.txt
     pip install -U ..
     git checkout requirements.txt
     if [ ! -z "$2" ]
