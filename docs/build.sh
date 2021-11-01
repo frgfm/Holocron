06c6d3f738181e34b4d19167a059c0b18ca8563d
frgfm <fgfm03@hotmail.fr>
2021-11-01 02:47:34 +0100

    docs: Fix doc building

--- docs/build.sh
+++ docs/build.sh
@@ -6,9 +6,9 @@ function deploy_doc(){
     COMMIT=$(git rev-parse --short HEAD)
     echo "Creating doc at commit" $COMMIT "and pushing to folder $2"
     # Hotfix
-    sed -i "s/^torchvision.*/&,<0.11.0/" requirements.txt
+    sed -i "s/^torchvision.*/&,<0.11.0/" ../requirements.txt
     pip install -U ..
-    git checkout requirements.txt
+    git checkout ../requirements.txt
     if [ ! -z "$2" ]
     then
         if [ "$2" == "latest" ]; then
