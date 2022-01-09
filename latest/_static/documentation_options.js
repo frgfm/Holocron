210e120bee91662ae8f470f9e8ff24648ccab25e
frgfm <fgfm03@hotmail.fr>
2022-01-09 22:54:54 +0000

    [skip ci] Documentation updates

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0.dev0+bc94c16-git',
+    VERSION: '0.2.0.dev0+bc6be2a-git',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
