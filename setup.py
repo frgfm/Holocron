ee3ebb1920fab3288b970cac941ddb08a74119ec
frgfm <fgfm03@hotmail.fr>
2022-02-05 14:14:57 +0100

    feat: Added HF hub model creation

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ _deps = [
     "fastprogress>=1.0.0",
     "matplotlib>=3.0.0",
     "Pillow>=8.4.0",  # cf. https://github.com/pytorch/vision/issues/4934
+    "huggingface-hub>=0.4.0",
     # Testing
     "pytest>=5.3.2",
     "coverage>=4.5.4",
