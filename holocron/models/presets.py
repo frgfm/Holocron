4cca4c67637f37464ecf0f12cad47a567773ed65
frgfm <26927750+frgfm@users.noreply.github.com>
2022-06-27 13:57:43 +0200

    docs: Fixed header

--- holocron/models/presets.py
+++ holocron/models/presets.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018, François-Guillaume Fernandez.
+# Copyright (C) 2022, François-Guillaume Fernandez.
 
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
