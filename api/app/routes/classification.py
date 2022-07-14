569e083acbaddc20cd9c67794aedd215590b32bd
frgfm <26927750+frgfm@users.noreply.github.com>
2022-07-14 19:59:50 +0200

    refactor: Changed backend to ONNX

--- api/app/routes/classification.py
+++ api/app/routes/classification.py
@@ -3,11 +3,11 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
-import torch
+
 from fastapi import APIRouter, File, UploadFile, status
 
 from app.schemas import ClsCandidate
-from app.vision import classification_model, classification_preprocessor, decode_image
+from app.vision import MODEL_CFG, classify_image, decode_image
 
 router = APIRouter()
 
@@ -15,11 +15,9 @@ router = APIRouter()
 @router.post("/", response_model=ClsCandidate, status_code=status.HTTP_200_OK, summary="Perform image classification")
 async def classify(file: UploadFile = File(...)):
     """Runs holocron vision model to analyze the input image"""
-    img_tensor = classification_preprocessor(decode_image(file.file.read()))
-    out = classification_model(img_tensor.unsqueeze(0)).squeeze(0)
-    probs = torch.softmax(out, dim=0)
+    probs = classify_image(decode_image(file.file.read()))
 
     return ClsCandidate(
-        value=classification_model.default_cfg["classes"][probs.argmax().item()],
-        confidence=probs.max().item(),
+        value=MODEL_CFG["classes"][probs.argmax()],
+        confidence=float(probs.max()),
     )
