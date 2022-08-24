86aa70384187c99cdafb2af4a3666126d6b7afc2
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-08-24 18:07:01 +0200

    test: Updated unittests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,9 @@
+import numpy as np
 import pytest
 import torch
-from torch.utils.data import DataLoader, Dataset
+from PIL import Image
 
 from holocron import utils
-
-
-class MockDataset(Dataset):
-    """Mock dataset generating a random sample and a fixed zero target"""
-
-    def __init__(self, n):
-        super().__init__()
-        self.n = n
-
-    def __getitem__(self, idx):
-        return torch.rand(32), 0
-
-    def __len__(self):
-        return self.n
 
 
 def test_mixup():
@@ -63,43 +50,19 @@ def test_mixup():
     assert mix_target.dtype == torch.float32 and mix_target.shape == (batch_size, num_classes)
 
 
-def _train_one_batch(model, x, target, optimizer, criterion, device):
-    """Mock batch training function"""
+@pytest.mark.parametrize(
+    "arr, fn, expected, progress, num_threads",
+    [
+        [[1, 2, 3], lambda x: x**2, [1, 4, 9], False, 3],
+        [[1, 2, 3], lambda x: x**2, [1, 4, 9], True, 1],
+        ["hello", lambda x: x.upper(), list("HELLO"), True, None],
+        ["hello", lambda x: x.upper(), list("HELLO"), False, None],
+    ],
+)
+def test_parallel(arr, fn, expected, progress, num_threads):
+    assert utils.parallel(fn, arr, progress=progress, num_threads=num_threads) == expected
 
-    x, target = x.to(device), target.to(device)
-    output = model(x)
-    batch_loss = criterion(output, target)
 
-    optimizer.zero_grad()
-    batch_loss.backward()
-    optimizer.step()
-
-    return batch_loss.item()
-
-
-def test_lr_finder():
-    num_it = 10
-    batch_size = 2
-    start_lr, end_lr = 1e-7, 10
-    # Generate all dependencies
-    model = torch.nn.Linear(32, 5)
-    train_loader = DataLoader(MockDataset(num_it * batch_size), batch_size=batch_size)
-    optimizer = torch.optim.Adam(model.parameters())
-    criterion = torch.nn.CrossEntropyLoss()
-    # Perform the iterations
-    lrs, losses = utils.misc.lr_finder(
-        _train_one_batch,
-        model,
-        train_loader,
-        optimizer,
-        criterion,
-        num_it=num_it,
-        start_lr=start_lr,
-        end_lr=end_lr,
-        stop_div=False,
-    )
-
-    # Check integrity of results
-    assert isinstance(lrs, list) and isinstance(losses, list)
-    assert len(lrs) == len(losses) == num_it
-    assert lrs[0] == start_lr and abs(lrs[-1] - end_lr) / lrs[-1] < 1e-7
+def test_find_image_size():
+    ds = [(Image.fromarray(np.full((16, 16, 3), 255, dtype=np.uint8)), 0) for _ in range(100)]
+    utils.find_image_size(ds, block=False)
