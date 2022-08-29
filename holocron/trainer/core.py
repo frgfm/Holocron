fix: Fixed the gradient clipping

holocron/trainer/core.py
@@ -187,11 +187,11 @@ class Trainer:
         if self.amp:
             # Backprop
             self.scaler.scale(loss).backward()
-            # Safeguard for Gradient explosion
-            if isinstance(self.grad_clip, float):
-                self.scaler.unscale_(self.optimizer)
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_clip)  # type: ignore[attr-defined]
             if self._grad_count == self.gradient_acc:
+                # Safeguard for Gradient explosion
+                if isinstance(self.grad_clip, float):
+                    self.scaler.unscale_(self.optimizer)
+                    nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_clip)  # type: ignore[attr-defined]
                 self.scaler.step(self.optimizer)
                 self.scaler.update()
                 self.optimizer.zero_grad()
@@ -199,10 +199,10 @@ class Trainer:
         else:
             # Backprop
             loss.backward()
-            # Safeguard for Gradient explosion
-            if isinstance(self.grad_clip, float):
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_clip)  # type: ignore[attr-defined]
             if self._grad_count == self.gradient_acc:
+                # Safeguard for Gradient explosion
+                if isinstance(self.grad_clip, float):
+                    nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_clip)  # type: ignore[attr-defined]
                 self.optimizer.step()
                 self.optimizer.zero_grad()
                 self._grad_count = 0