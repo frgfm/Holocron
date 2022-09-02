ae285898d625b6d0797e70ec5addf74a4b8dc191
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-09-02 16:27:40 +0200

    docs: Added Adan equation in docstring

--- holocron/optim/adan.py
+++ holocron/optim/adan.py
@@ -12,8 +12,35 @@ from . import functional as F
 
 
 class Adan(Adam):
-    """Implements the Adan optimizer from `"Adan: Adaptive Nesterov Momentum Algorithm for Faster Optimizing Deep
+    r"""Implements the Adan optimizer from `"Adan: Adaptive Nesterov Momentum Algorithm for Faster Optimizing Deep
     Models" <https://arxiv.org/pdf/2208.06677.pdf>`_.
+
+    The estimation of momentums is described as follows, :math:`\forall t \geq 1`:
+
+    .. math::
+        m_t \leftarrow \beta_1 m_{t-1} + (1 - \beta_1) g_t \\
+        v_t \leftarrow \beta_2 v_{t-1} + (1 - \beta_2) (g_t - g_{t-1}) \\
+        n_t \leftarrow \beta_3 n_{t-1} + (1 - \beta_3) [g_t + \beta_2 (g_t - g_{t - 1})]^2
+
+    where :math:`g_t` is the gradient of :math:`\theta_t`,
+    :math:`\beta_1, \beta_2, \beta_3 \in [0, 1]^3` are the exponential average smoothing coefficients,
+    :math:`m_0 = g_0,\ v_0 = 0,\ n_0 = g_0^2`.
+
+    Then we correct their biases using:
+
+    .. math::
+        \hat{m_t} \leftarrow \frac{m_t}{1 - \beta_1^t} \\
+        \hat{v_t} \leftarrow \frac{v_t}{1 - \beta_2^t} \\
+        \hat{n_t} \leftarrow \frac{n_t}{1 - \beta_3^t}
+
+    And finally the update step is performed using the following rule:
+
+    .. math::
+        p_t \leftarrow \frac{\hat{m_t} + (1 - \beta_2) \hat{v_t}}{\sqrt{\hat{n_t} + \epsilon}} \\
+        \theta_t \leftarrow \frac{\theta_{t-1} - \alpha p_t}{1 + \lambda \alpha}
+
+    where :math:`\theta_t` is the parameter value at step :math:`t` (:math:`\theta_0` being the initialization value),
+    :math:`\alpha` is the learning rate, :math:`\lambda \geq 0` is the weight decay, :math:`\epsilon > 0`.
 
     Args:
         params (iterable): iterable of parameters to optimize or dicts defining parameter groups
@@ -22,7 +49,6 @@ class Adan(Adam):
         eps (float, optional): term added to the denominator to improve numerical stability (default: 1e-8)
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
         amsgrad (bool, optional): whether to use the AMSGrad variant (default: False)
-        delta (float, optional): delta threshold for projection (default: False)
     """
 
     def __init__(
